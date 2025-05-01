166ce2ae53fe8f36ffbf3ba1282e58d623692671
Dhara Bhatti <bhattidhara166@gmail.com>
2025-05-01 19:00:30 +0530

    Added header changes in web

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -1,10 +1,18 @@
 "use client";
 import * as React from "react";
-import { useState } from "react";
-import { Button } from "./Button";
+import {useState} from "react";
+import {Button} from "./Button";
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import {Link, usePage} from "@inertiajs/react";
+import Dropdown from "@/Components/Dropdown";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 
 export const Header = () => {
+    const user = usePage().props.auth.user;
+
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [showingNavigationDropdown, setShowingNavigationDropdown] =
+        useState(false);
     let arrHeaderLinks = [
         {
             title: "Home",
@@ -17,6 +25,10 @@ export const Header = () => {
         {
             title: "About Us",
             url: "/about-us",
+        },
+        {
+            title: "Privacy Policy",
+            url: "/privacy-policy",
         },
         {
             title: "Contact Us",
@@ -33,20 +45,14 @@ export const Header = () => {
         </a>
     ));
     return (
-        <header className="fixed top-0 px-5 py-3 w-full bg-white border-b border-solid border-b-zinc-800 z-[100] max-md:px-5 max-md:py-3">
+        <header
+            className="fixed top-0 px-5 py-3 w-full bg-white border-b border-solid border-b-zinc-800 z-[100] max-md:px-5 max-md:py-3">
             <div className="flex relative justify-between items-center mx-auto my-0 max-w-[1200px]">
-                <a href="/" className="flex gap-2.5 items-center">
-                    <img
-                        alt="Logo"
-                        src="https://images.pexels.com/photos/31763558/pexels-photo-31763558.jpeg"
-                        className="object-cover overflow-hidden aspect-square w-[25px]"
-                    />
-                    <span className="text-lg font-medium text-neutral-500">
-                        builder.io
-                    </span>
-                </a>
+                <Link href="/">
+                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800"/>
+                </Link>
                 <button
-                    className="p-1.5 text-2xl cursor-pointer border-none text-white lg:hidden"
+                    className="p-1.5 text-2xl cursor-pointer border-none text-gray-600 lg:hidden"
                     onClick={() => setIsMenuOpen(!isMenuOpen)}
                     aria-expanded={isMenuOpen}
                     aria-label="Toggle menu"
@@ -65,15 +71,85 @@ export const Header = () => {
                 <div
                     className={`flex gap-2 lg:flex ${
                         isMenuOpen
-                            ? "absolute top-[calc(100%+200px)] left-0 right-0 flex-col items-center p-5 bg-black border-b border-zinc-800 animate-fadeIn"
+                            ? "absolute top-[calc(100%+200px)] left-0 right-0 flex-col lg:items-center p-5 bg-black border-b border-zinc-800 animate-fadeIn"
                             : "hidden lg:flex"
                     }`}
                 >
-                    <a href="/register">
-                        <Button variant="solid" className="w-full lg:w-auto">
-                            Sign up
-                        </Button>
-                    </a>
+                    {user ? (
+                        <div className="lg:mx-auto lg:max-w-7xl lg:px-4 sm:px-6 lg:px-8">
+                            <div className="flex h-16 justify-between">
+                                <div className="hidden sm:ms-6 sm:flex sm:items-center">
+                                    <div className="relative ms-3">
+                                        <Dropdown>
+                                            <Dropdown.Trigger>
+                                                <span className="inline-flex rounded-md">
+                                                    <button
+                                                        type="button"
+                                                        className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+                                                    >
+                                                        {user.name}
+
+                                                        <svg
+                                                            className="-me-0.5 ms-2 h-4 w-4"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            viewBox="0 0 20 20"
+                                                            fill="currentColor"
+                                                        >
+                                                            <path
+                                                                fillRule="evenodd"
+                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                                clipRule="evenodd"
+                                                            />
+                                                        </svg>
+                                                    </button>
+                                                </span>
+                                            </Dropdown.Trigger>
+
+                                            <Dropdown.Content>
+                                                <Dropdown.Link
+                                                    href={route('profile.edit')}
+                                                >
+                                                    Profile
+                                                </Dropdown.Link>
+                                                <Dropdown.Link
+                                                    href={route('logout')}
+                                                    method="post"
+                                                    as="button"
+                                                >
+                                                    Log Out
+                                                </Dropdown.Link>
+                                            </Dropdown.Content>
+                                        </Dropdown>
+                                    </div>
+                                </div>
+                                <div className="mt-3 space-y-1 sm:hidden">
+                                    <ResponsiveNavLink href={route('profile.edit')}>
+                                        Profile
+                                    </ResponsiveNavLink>
+                                    <ResponsiveNavLink
+                                        method="post"
+                                        href={route('logout')}
+                                        as="button"
+                                    >
+                                        Log Out
+                                    </ResponsiveNavLink>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <>
+                            <a href="/login">
+                                <Button variant="solid" className="w-full lg:w-auto">
+                                    Login
+                                </Button>
+                            </a>
+                            <a href="/register">
+                                <Button variant="solid" className="w-full lg:w-auto">
+                                    Sign up
+                                </Button>
+                            </a>
+                        </>
+                    )}
                 </div>
             </div>
         </header>
