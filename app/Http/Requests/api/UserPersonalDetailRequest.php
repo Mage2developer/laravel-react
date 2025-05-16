c4c1d67d5c3d5a867a3f1657181a21a287600540
Yogesh Shishangiya <yogesh@briteskies.com>
2025-05-16 17:30:18 +0530

    Number validation

--- app/Http/Requests/api/UserPersonalDetailRequest.php
+++ app/Http/Requests/api/UserPersonalDetailRequest.php
@@ -25,7 +25,7 @@ class UserPersonalDetailRequest extends FormRequest
             'dob' => 'required|string|max:50',
             'marital_status' => 'required|boolean|max:5',
             'height' => 'required|string|max:15',
-            'weight' => 'required|number|max:15',
+            'weight' => 'required|string|max:15',
             'manglik' => 'boolean',
             'have_specs' => 'boolean',
             'hobby' => 'string|max:500',
