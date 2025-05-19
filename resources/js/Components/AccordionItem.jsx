b2f0925dc173b68d816c8859138818750b1010c3
Yogesh Shishangiya <yogesh@briteskies.com>
2025-05-19 20:33:34 +0530

    master

--- resources/js/Components/AccordionItem.jsx
+++ resources/js/Components/AccordionItem.jsx
@@ -3,7 +3,7 @@ import {FaChevronUp, FaChevronDown} from "react-icons/fa";
 
 const AccordionItem = React.memo(({ item, isOpen, onClick }) => {
     return (
-        <div className="bg-white p-4 mb-3 shadow sm:rounded-lg sm:p-8">
+        <div className="border shadow-lg rounded-xl bg-white p-4 mb-3 shadow sm:rounded-lg sm:p-8">
             <button onClick={() => onClick(item.id)} className="w-full">
                 <h2 className="text-xl font-medium text-gray-900 hover:text-[#ff3131] flex items-center justify-between">
                     {item.title} {isOpen ? <FaChevronUp/> : <FaChevronDown/>}
--- resources/js/Pages/Admin/Users/Index.jsx
+++ resources/js/Pages/Admin/Users/Index.jsx
@@ -108,7 +108,7 @@ export default function Index({ users, filters }) {
             <Head title="Users" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 border shadow-lg rounded-xl">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
                             <h1 className="text-2xl font-semibold mb-6">User Management</h1>
