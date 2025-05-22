7c0f512e42a405395689543e4ad6ff9e4adec7d6
Yogesh Shishangiya <yogesh@briteskies.com>
2025-05-22 11:17:12 +0530

    Remove lazy load for logo

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,9 +1,7 @@
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
 import React from "react";
 
 export default function ApplicationLogo(props) {
-    return <LazyLoadImage
+    return <img
         alt="Vanand Vivah Logo"
         src="/images/header-logo.webp"
         effect="blur"
