115727523778ea28bb73b33a22978106c68980e4
Yogesh Shishangiya <you@example.com>
2025-05-29 19:22:51 +0530

    Changes in Loading images

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 export default function ApplicationLogo(props) {
-    return <img
-        alt="Vanand Vivah Logo"
-        src="/images/header-logo.webp"
-        effect="blur"
-        height={"70"}
-        width={"234"}
-    />
+    return (
+        <img
+            alt="Vanand Vivah Logo"
+            src="/images/header-logo.webp"
+            effect="opacity"
+            height={"70"}
+            width={"234"}
+            loading="eager"
+            threshold={300}
+            wrapperClassName="h-[70px] w-[234]"
+        />
+    );
 }
