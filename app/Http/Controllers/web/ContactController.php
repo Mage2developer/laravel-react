23291bf200cda6ebbc901ac9b4af7f4ef21a55c5
Ubuntu <ubuntu@ip-172-31-21-101.ap-southeast-1.compute.internal>
2025-06-02 13:33:42 +0000

    Made email sending functionality working on server

--- app/Http/Controllers/web/ContactController.php
+++ app/Http/Controllers/web/ContactController.php
@@ -5,7 +5,12 @@ namespace App\Http\Controllers\web;
 use App\Events\ContactEvent;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\web\Auth\ContactRequest;
+use App\Mail\ContactForm;
 use Inertia\Inertia;
+
+use Illuminate\Support\Facades\Log;
+
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -22,9 +27,11 @@ class ContactController extends Controller
 
         event(new ContactEvent($data));
 
-        // Send the email
-        /*Mail::to(config('mail.from.address')) // Configure your recipient email in config/mail.php
-        ->send(new ContactFormSubmitted($request->all()));*/
+        try {
+            Mail::to(config('mail.from.address'))->send(new ContactForm($request->all()));  
+        } catch(Exception $exception) {
+            Log::critical($exception->getMessage());
+        }
 
         // Optionally, you can redirect the user with a success message
         return redirect()->back()->with('success', 'Your message has been sent successfully.');
