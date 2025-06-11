1b5e37e049bee1895505132302aef9ded8ace181
yogesh shishangiya <shishangiya.yogesh@gmail.com>
2025-06-11 12:16:32 +0530

    Slider changes

--- resources/js/Components/Home/HeroSlider.jsx
+++ resources/js/Components/Home/HeroSlider.jsx
@@ -13,33 +13,26 @@ const sliderImages = [
         title: "Find your perfect match with Vanand Vivah",
         description:
             "At Vanand Vivah, we connect brides and grooms to help them discover meaningful relationships. Join our community today.",
+        srcSet: "/images/home/slider/mobile/wedding-banner.webp 480w,/images/home/slider/wedding-banner.webp 1900w",
+        sizes: "(max-width: 480px) 100vw, (max-width: 768px) 100vw, 100vw"
     },
     {
         src: "/images/home/slider/destination-wedding.webp",
         title: "Join thousands finding love",
         description:
             "Trusted by thousands across the world, Vanand Vivah helps you take the first step toward a fulfilling relationship.",
+        srcSet: "/images/home/slider/mobile/destination-wedding.webp 480w,/images/home/slider/destination-wedding.webp 1900w",
+        sizes: "(max-width: 480px) 100vw, (max-width: 768px) 100vw, 100vw"
     },
     {
         src: "/images/home/slider/hindu-wedding-mandap.webp",
         title: "Safe, secure, and personalized",
         description:
             "We value your privacy and provide you with a secure platform tailored to your unique preferences.",
+        srcSet: "/images/home/slider/mobile/hindu-wedding-mandap.webp 480w,/images/home/slider/hindu-wedding-mandap.webp 1900w",
+        sizes: "(max-width: 480px) 100vw, (max-width: 768px) 100vw, 100vw"
     },
 ];
-
-const preloadImages = async (imageUrls) => {
-    const promises = imageUrls.map(src => {
-        return new Promise((resolve, reject) => {
-            const img = new Image();
-            img.src = src;
-            img.onload = () => resolve();
-            img.onerror = () => reject();
-        });
-    });
-    await Promise.all(promises);
-}
-
 function HeroSlider() {
     const user = usePage().props.auth.user;
 
@@ -66,11 +59,10 @@ function HeroSlider() {
                                     alt="Vanand Vivah Registration"
                                     src={slide.src}
                                     effect="eager"
-                                    srcSet="/images/home/slider/mobile/wedding-banner.webp 480w,/images/home/slider/wedding-banner.webp 1900w"
+                                    srcSet= {slide.srcSet}
                                     className="object-cover w-full h-full"
-                                    width={"100%"}
                                     height={"600px"}
-                                    sizes="(max-width: 480px) 100vw, (max-width: 768px) 100vw, 100vw"
+                                    sizes={slide.sizes}
                                 />
                             ) : (
                                 <LazyLoadImage
@@ -78,8 +70,9 @@ function HeroSlider() {
                                     src={slide.src}
                                     effect="blur"
                                     className="object-cover w-full h-full"
-                                    width={"100%"}
                                     height={"600px"}
+                                    sizes={slide.sizes}
+                                    srcSet= {slide.srcSet}
                                 />
                             )}
                             <div className="absolute top-2/4 left-2/4 px-5 py-0 w-full text-center -translate-x-2/4 -translate-y-2/4 max-w-[800px]">
