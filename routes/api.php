c063c3757daa9161ab5a97c09fd9335651e2c5c9
Yogesh Shishangiya <yogesh@briteskies.com>
2025-06-23 16:32:47 +0530

    Address Changes API

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Http\Controllers\api\ContactController;
+use App\Http\Controllers\api\ForgotPasswordController;
+use App\Http\Controllers\api\GetCityController;
+use App\Http\Controllers\api\GetCountryController;
+use App\Http\Controllers\api\GetStateController;
 use App\Http\Controllers\api\GetUserProfileController;
 use App\Http\Controllers\api\UserAuthController;
 use App\Http\Controllers\api\UserContactDetailController;
 use App\Http\Controllers\api\UserEducationDetailController;
 use App\Http\Controllers\api\UserFamilyDetailController;
+use App\Http\Controllers\api\UserImageController;
+use App\Http\Controllers\api\UserPasswordUpdate;
 use App\Http\Controllers\api\UserPersonalDetailController;
 use App\Http\Controllers\api\UserProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\api\UserImageController;
-use App\Http\Controllers\api\ForgotPasswordController;
-use App\Http\Controllers\api\ContactController;
-use App\Http\Controllers\api\UserPasswordUpdate;
 
 
 Route::get('/user', function (Request $request) {
@@ -24,6 +27,10 @@ Route::post("/login", [UserAuthController::class, 'login']);
 Route::post("/signup", [UserAuthController::class, 'signup']);
 
 Route::post('/contact-us', [ContactController::class, 'createRequest'])->name('contact.submit');
+
+Route::get('/getState', [GetStateController::class, 'show'])->name('state.index');
+Route::get('/getCountry', [GetCountryController::class, 'show'])->name('country.index');
+Route::get('/getCity', [GetCityController::class, 'show'])->name('city.index');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [UserProfileController::class, 'show']);
