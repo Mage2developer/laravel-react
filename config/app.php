fc2d9ad13c30e45574f0d9be304e13bb8c45ce4a
Vanand Vivah <vanandvivah@gmail.com>
2025-06-26 07:43:09 +0000

    Change site title

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Vanand-Vivah'),
+    'name' => env('APP_NAME', 'Vanand Vivah'),
 
     /*
     |--------------------------------------------------------------------------
